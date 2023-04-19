59b13f57c20f07309817c8c48001fbc389d17747
kimjoemaina <joemainak@gmail.com>
2023-04-19 21:54:57 +0300

    update new port

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ app.get('/**', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 10000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
